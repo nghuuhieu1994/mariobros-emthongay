b7d267007aa7a7a1db288150048a12bfd5b25064
emthichhocit94 <emthichhocit94@gmail.com>
2014-06-04 10:18:22 +0000

    + update mapeditor no bugs

--- Map Editor/Map Editor/LoadSave.cs
+++ Map Editor/Map Editor/LoadSave.cs
@@ -68,7 +68,7 @@ namespace Map_Editor
 
             //Vector tmpVector = Management.GetBoundOfMap(cvMap, ref width, ref height);
             //Management.TranslatePosition(cvMap, -(int)tmpVector.X, (int)tmpVector.Y);
-
+            //width = Management.GetHorBoundOfMap(cvMap);
 
             int dimension;
             if (width > height)
