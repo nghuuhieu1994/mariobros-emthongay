c4ebad50ef3d96034c43fde316da233af2e50a1d
nghuuhieu1994 <nghuuhieu1994@b2608138-1ec9-ef53-e865-994afe2663b0>
2014-06-05 04:00:38 +0000

    [Hieu] set load game hiển thị đúng số màn chơi hiện tại

--- Mario Bros/Mario Bros/Mario Bros/State/LoadGame.cs
+++ Mario Bros/Mario Bros/Mario Bros/State/LoadGame.cs
@@ -14,7 +14,9 @@ namespace Mario_Bros.State
 {
     class LoadGame : GameState
     {
+        private CSprite Icon;
         private float CoolDown = 0;
+        private string World_Name;
         public LoadGame(IDGameState _ID)
             : base(_ID)
         {
@@ -22,6 +24,8 @@ namespace Mario_Bros.State
         public override void InitState(ContentManager CM)
         {
             MediaPlayer.Stop();
+            Icon = new CSprite(CResourceManager.GetInstance().GetResource(IDResource.ICON_COIN));
+            Icon.Position = new Vector2(145, 10);
             base.InitState(CM);
         }
 
@@ -39,13 +43,25 @@ namespace Mario_Bros.State
         }
         public override void Update(GameTime gameTime)
         {
+            //Icon.UpdateAnimation(gameTime);
             base.Update(gameTime);
         }
         public override void Draw(SpriteBatch SB)
         {
+            World_Name = GlobalSetting.m_IDLevel.ToString() + "-1";
             SB.Begin();
+            
             CResourceManager.GetInstance().GetResource(IDResource.LOAD).Draw(SB);
             StateManager.StringDrawer(GlobalValue.MARIO_LIFE.ToString(), new Vector2(192, 108), SB, Color.White);
+            StateManager.StringDrawer("MARIO", new Vector2(0, 0), SB, Color.White);
+            StateManager.StringDrawer(GlobalValue.MARIO_SCORE.ToString().PadLeft(6, '0'), new Vector2(0, 8), SB, Color.White);
+            StateManager.StringDrawer("WORLD", new Vector2(200, 0), SB, Color.White);
+            StateManager.StringDrawer(World_Name, new Vector2(205, 8), SB, Color.White);
+            StateManager.StringDrawer("TIME", new Vector2(336, 0), SB, Color.White);
+            //StateManager.StringDrawer(((int)(MathHelper.Clamp(GlobalValue.TIME_LEFT, 0, 400))).ToString(), new Vector2(340, 8), SB, Color.White);
+            StateManager.StringDrawer(" * ", new Vector2(150, 10), SB, Color.White);
+            StateManager.StringDrawer(GlobalValue.MARIO_COIN.ToString().PadLeft(2, '0'), new Vector2(162, 9), SB, Color.White);
+            Icon.Draw(SB);
             SB.End();
             base.Draw(SB);
         }
